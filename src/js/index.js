11606efd3f93edb4f037f6fd77aa41990d3b3230
SeanKeane6 <71730648+SeanKeane6@users.noreply.github.com>
2021-03-22 22:36:07 -0400

    added in base questions

---
+++ src/js/index.js
@@ -0,0 +1,29 @@
+DOMSelectors = {
+    displayContainer: document.querySelector("container"),
+};
+
+console.log(DOMSelectors.displayContainer);
+
+const questions = [
+{
+question : "this is a sample",
+choiceA : 'sample',
+choiceB : 'sample',
+choiceC : 'sample',
+correct : "B"
+},
+{
+question : "this is a sample",
+choiceA : 'sample',
+choiceB : 'sample',
+choiceC : 'sample',
+correct : "A"
+},
+{
+question : "this is a sample",
+choiceA : 'sample',
+choiceB : 'sample',
+choiceC : 'sample',
+correct : "B"
+}
+];
