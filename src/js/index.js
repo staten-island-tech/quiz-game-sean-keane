8a18c375b8a402685f9563e172393d210a0c6d2b
SeanKeane6 <71730648+SeanKeane6@users.noreply.github.com>
2021-04-15 00:07:04 -0400

    scrapping this
    Moving to the Web Dev video.

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,17 @@
 DOMSelectors = {
     displayContainer: document.querySelector(".container"),
-    finishButton: document.querySelector (".finish-btn"),
     scoreCard: document.querySelector(".scorecard"),
-    btn: document.querySelector(".btn"),
+    finishButton: document.getElementById ("finish-btn").addEventListener("click", displayScore),
+    buttonLog: document.querySelector(".choice-btn"),
+    
+    //correctChoiceButtons: document.getElementById(".choice-btn1", ".choice-btn2", ".choice-btn3", ".choice-btn4").addEventListener("click", newScore),
 };
+
+let score = 0;
 
 const questions = [
 {
-question : "this is a sample 3",
+question : "this is a sample 4",
 answers: 
     {
     text1: 'HTML', 
@@ -19,7 +23,7 @@ correctChoice: 'text2',
 
 },
 {
-question : "this is a sample 2",
+question : "this is a sample 3",
 answers: {
     text1: 'HTML', 
     text2: 'sample2', 
@@ -30,7 +34,7 @@ correctChoice: 'text4',
 
 },
 {
-question : "this is a sample",
+question : "this is a sample2",
 answers: {
     text1: 'HTML', 
     text2: 'sample1', 
@@ -40,54 +44,50 @@ answers: {
 correctChoice: 'text1',
 
 },
+{
+question : "this is a sample",
+answers: {
+    text1: 'HTML', 
+    text2: 'sample1', 
+    text3: 'gibbons',
+    text4: 'sample1', 
+    },
+correctChoice: 'text3',
+    
+},
 ];
 
-let scoreCard = document.getElementById(".scorecard");
+const init = function() {
 
-const init = function(scoreCard){
+    
 
-questions.forEach((item) =>
+    questions.forEach((item) =>
     DOMSelectors.displayContainer.insertAdjacentHTML(
         "afterbegin",
         `<div id="question-container">
             <div id="question">${item.question}</div>
             <div id="answer-buttons" class="btn-grid">
-                <button class="btn">${item.answers.text1}</button>
-                <button class="btn">${item.answers.text2}</button>
-                <button class="btn">${item.answers.text3}</button>
-                <button class="btn">${item.answers.text4}</button>
+                <button id="choice-btn">${item.answers.text1}</button>
+                <button id="choice-btn">${item.answers.text2}</button>
+                <button id="choice-btn">${item.answers.text3}</button>
+                <button id="choice-btn">${item.answers.text4}</button>
             </div>`
-),
-),
+      ),
+    );
+};
 
-//console.log(scoreCard),
+function newScore(item) {
+    if (item.target.id === questions.correctChoice) {
+        score + 1;
+    };
 
-scoreCard.classList.style.visibility = "hidden";
+    return newScore
+}
+
+ function displayScore() {
+
+document.getElementById("score").innerHTML = "pog"
 
 }; 
 
-/* This inserts the objects and arrays into the HTML */
-
-init(); 
-
-//btn.addEventListener('click');
-
- /* const displayScore = function(){
-    const correctAnswerLog = [i++];
-    const answers = [4];
-
-    for(const i=0; i<questions.length; i){
-        answers = [];
-    };
-
-    output.push (
-    questions.forEach((answer) => DOMSelectors.scoreCard.insertAdjacentHTML(`<div class="score">${answers.join('')}/3</div>`)
-    ),
-
-
-
-};
-
-function showResult(btn){
-
-} */
+init();
