9d4a331530df6db55cce49df50c70f653ba87364
SeanKeane6 <71730648+SeanKeane6@users.noreply.github.com>
2021-04-20 10:00:46 -0400

    class

--- src/js/index.js
+++ src/js/index.js
@@ -1,43 +1,42 @@
 DOMSelectors = {
     displayContainer: document.querySelector(".container"),
+    displayScore: document.querySelector(".score"),
 
-    correctChoiceButtons: document.getElementById(".choice-btn1").addEventListener("click", scoreLog),
-    correctChoiceButtons: document.getElementById(".choice-btn1").addEventListener("click", scoreLog),
-    correctChoiceButtons: document.getElementById(".choice-btn1").addEventListener("click", scoreLog),
+    //correctChoiceButtons: init.getElementById(".choice-btn").addEventListener("click", scoreLog),
 
     // NOT WORKING
 
-    scoreCard: document.getElementById(".scorecard"),
+    //scoreCard: document.getElementById(".scorecard").addEventListener(displayScore),
 
-    finishButton: document.getElementById ("finish-btn").addEventListener("click", displayScore),
+    //finishButton: document.getElementById ("finish-btn").addEventListener("click", displayScore),
 };
 
 const questions = [
 {
-question : "this is a sample3",
+question : "this is a sample",
 answers: [
-    {text1: 'HTML', correct: false},
-    {text2: 'sample3', correct: false},
-    {text3: 'css', correct: true},
-    {text4: 'sample3', correct: false},
+    {text: 'HTML', correct: false},
+    {text: 'sample3', correct: false},
+    {text: 'css', correct: true},
+    {text: 'sample3', correct: false},
 ],
 },
 {
 question : "this is a sample 2",
 answers: [
-    {text1: 'sample2', correct: true},
-    {text2: 'sample2', correct: false},
-    {text3: 'sample2', correct: false},
-    {text4: 'sample2', correct: false},
+    {text: 'sample2', correct: true},
+    {text: 'sample2', correct: false},
+    {text: 'sample2', correct: false},
+    {text: 'sample2', correct: false},
 ],
 },
 {
-question : "this is a sample",
+question : "this is a sample3",
 answers: [
-    {text1: 'sampleb', correct: false},
-    {text2: 'samplea', correct: false},
-    {text3: 'sample1', correct: false},
-    {text4: 'samplec', correct: true},
+    {text: 'sampleb', correct: false},
+    {text: 'samplea', correct: false},
+    {text: 'sample1', correct: false},
+    {text: 'samplec', correct: true},
 ],
 },
 ];
@@ -49,10 +48,10 @@ DOMSelectors.displayContainer.insertAdjacentHTML(
         `<div id="question-container">
             <div id="question">${item.question}</div>
             <div id="answer-buttons" class="btn-grid">
-                <button id="choice-btn">${item.answers[0].text1}</button>
-                <button id="choice-btn">${item.answers[1].text2}</button>
-                <button id="choice-btn">${item.answers[2].text3}</button>
-                <button id="choice-btn">${item.answers[3].text4}</button>
+                <button id="choice-btn">${item.answers[0].text}</button>
+                <button id="choice-btn">${item.answers[1].text}</button>
+                <button id="choice-btn">${item.answers[2].text}</button>
+                <button id="choice-btn">${item.answers[1].text}</button>
             </div>`
 )
 );
@@ -60,14 +59,18 @@ DOMSelectors.displayContainer.insertAdjacentHTML(
 
 init(); 
 
+
 let initialScore = 0;
 
 function scoreLog(e) {
- if (e.target.id === questions.answers.correct){
+ if (e.target.id === questions.answers.correct(true)){
      initialScore ++
  }
 } 
 
 function displayScore() {
     scoreCard.style.display = "Block";
+    scoreLog {
+
+    }
 }
