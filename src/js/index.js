eefd301fabf60546e5c095741473b55a4e21701f
SeanKeane6 <71730648+SeanKeane6@users.noreply.github.com>
2021-04-23 01:49:24 -0400

    Added more functions that don't work

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ DOMSelectors = {
     displayContainer: document.querySelector(".container"),
     displayScore: document.querySelector(".score"),
 
+<<<<<<< Updated upstream
     //correctChoiceButtons: init.getElementById(".choice-btn").addEventListener("click", scoreLog),
 
     // NOT WORKING
@@ -9,6 +10,17 @@ DOMSelectors = {
     //scoreCard: document.getElementById(".scorecard").addEventListener(displayScore),
 
     //finishButton: document.getElementById ("finish-btn").addEventListener("click", displayScore),
+=======
+    //correctChoiceButtons: document.getElementById(".choice-btn").addEventListener("click"),
+
+    // NOT WORKING
+
+    finishBtn: document.querySelector ("hide"),
+
+    scoreCard: document.getElementById ("scorecard"),
+
+    finishBtn: document.querySelector ("finish-btn"),//.addEventListener("click", displayScore),
+>>>>>>> Stashed changes
 };
 
 const questions = [
@@ -62,15 +74,27 @@ init();
 
 let initialScore = 0;
 
+<<<<<<< Updated upstream
 function scoreLog(e) {
  if (e.target.id === questions.answers.correct(true)){
+=======
+function scoreLog(answers) {
+ if (answers.target.id === questions.answers.correct){
+>>>>>>> Stashed changes
      initialScore ++
  }
 } 
 
+<<<<<<< Updated upstream
 function displayScore() {
     scoreCard.style.display = "Block";
     scoreLog {
 
     }
 }
+=======
+
+  DOMSelectors.finishBtn.addEventListener("click", () => {
+   DOMSelectors.hide.classList.remove("hide");
+}); 
+>>>>>>> Stashed changes
